5108a437183407debc86f16cced38ed20cc9d409
yaambs <yanshbhardwaj14@gmail.com>
2021-11-02 17:19:47 +0530

    LinkedHashSet hand-on example codes added

---
+++ src/com/javacollections/linkedhashsetcodes/LinkedHashSetExample1.java
@@ -0,0 +1,22 @@
+package com.javacollections.linkedhashsetcodes;
+
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+class LinkedHashSetExample1{
+    public static void main(String[] args){
+        //Creating HashSet and adding elements
+        Set<String> set=new LinkedHashSet<>();
+        set.add("One");
+        set.add("Two");
+        set.add("Three");
+        set.add("Four");
+        set.add("Five");
+        Iterator<String> i=set.iterator();
+        while(i.hasNext())
+        {
+            System.out.println(i.next());
+        }
+    }
+}
